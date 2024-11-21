0bfcf627e215d9eb2a1b80afe3d45cfdfbc35d37
alec (- <alela841@student.liu.se>
2024-11-21 12:10:33 +0100

    poäng

---
+++ project/src/TaskPriority.jsx
@@ -0,0 +1,24 @@
+
+// "private"
+
+// getInput
+
+// calculateDeadline
+
+
+// calculateDifficulty
+
+// calculateTime
+
+
+// calculateTotal 
+// 1. Deadline + 2. Difficulty/Time
+
+
+
+
+// "public"
+
+// getTotal
+
+
