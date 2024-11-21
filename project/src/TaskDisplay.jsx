a39b87ab93ea26d657195fcc49a7412c1a7acde6
Randy Tran <rantr938@student.liu.se>
2024-11-21 13:32:16 +0100

    taskTest

--- project/src/TaskDisplay.jsx
+++ project/src/TaskDisplay.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 export default function TaskDisplay(){
+    // const taskDisplay = Task();
+    const taskDetails = {name: 'taskName', desc: 'taskDesc'};
     return(
         <div className = 'taskContainer'>
-            <div className></div>
+            <div className = 'task'>Today's tasks</div>
+            <Task data = {taskDetails}></Task>
         </div>
     )
         
+}
+
+function Task(props){
+    const {data} = props
+    console.log(data);
+    return(
+        <div className = "task">{data.name}</div>
+    )
 }
