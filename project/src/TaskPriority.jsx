9e1244669224d7f70948a1fa72cc8fc4caaf1dfe
alec (- <alela841@student.liu.se>
2024-11-21 15:52:40 +0100

    Update TaskPriority.jsx

--- project/src/TaskPriority.jsx
+++ project/src/TaskPriority.jsx
@@ -1,15 +1,32 @@
 
 // "private"
 
-// getInput
+// getInput?
 
-// calculateDeadline
+// beräknar prio-poäng utifrån 
+function calculateDeadlinePoints(daysLeft) {
 
+    return deadlinePoints
+}
 
-// calculateDifficulty
+// beräknar prio-poäng utifrån progress i %
+function calculateProgressPoints(progressValue) {
+    let progressPoints; 
+    for (let i = 1; i <= 5; i++) {
+        if (progressValue <= i*20) {
+            progressPoints = i; 
+        }
+    }
+    
+    return progressPoints; 
+
+}; 
+
 
 // calculateTime
 
+// sort priority [index 1 highest priority, index 5 lowest priority]
+ 
 
 // calculateTotal 
 // 1. Deadline + 2. Difficulty/Time
