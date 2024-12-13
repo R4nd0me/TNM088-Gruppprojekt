685d89f338bf3d28559ab8e858bf424a28639c43
Randy Tran <rantr938@student.liu.se>
2024-12-13 17:55:50 +0100

    Updated icons

--- project/src/NavigationBar.jsx
+++ project/src/NavigationBar.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
-
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AssignmentIcon from '@mui/icons-material/Assignment';
 export default function NavigationBar(){
 
     const navigate = useNavigate();
     return (
         <div className="navbar">
             <Link to = '/settings'>
-            <IconButton aria-label='Settings' size = 'large'><SettingsIcon></SettingsIcon></IconButton>
+            <IconButton aria-label='Settings'><SettingsIcon sx = {{fontSize:40}}></SettingsIcon></IconButton>
             </Link>
             <Link to = '/new'>
-            <button type = "button">New</button>
+            <IconButton aria-label='New'><AddCircleIcon sx = {{fontSize:40}}></AddCircleIcon></IconButton>
             </Link>
             <Link to = '/viewtasks'>
-            <button type = "button">See Tasks</button>
+            <IconButton aria-label='New'><AssignmentIcon sx = {{fontSize:40}}></AssignmentIcon></IconButton>
             </Link>
         </div>
     )
